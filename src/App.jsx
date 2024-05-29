ebeef3c473bce94b72404a3b0666411ae66c85d5
Adi <adithiyans123@gmail.com>
2024-05-29 09:07:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='text-4xl bg-orange-400'>Hi</div>
+    </>
+  )
+}
+
+export default App
