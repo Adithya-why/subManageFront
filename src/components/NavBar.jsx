2d9997d7937520b2cac83a23eeae05a43f79657c
Adi <adithiyans123@gmail.com>
2024-05-29 15:36:46 +0530

    used Link for routing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom"
+
+
 export default function NavBar(){
 
 
@@ -5,11 +8,11 @@ export default function NavBar(){
     return(
        <div className="navbar dm-sans-class w-full h-full bg-amber-800 flex items-center justify-evenly">
 
-            <div className=" text-3xl text-white text-center basis-2/3">Subman</div>
+            <div className=" text-3xl text-white text-center basis-2/3"><Link to={"/"}>Subman</Link></div>
 
             <div className=" text-white flex items-center gap-8 basis-1/3">
-                <button className=" p-2 bg-green-600 rounded">Log in</button>
-                <button className="p-2 bg-green-600 rounded">Sign up</button>
+                <Link to={"/login"}><button className=" p-2 bg-green-600 rounded">Log in</button></Link>
+                <Link to={"/register"}><button className="p-2 bg-green-600 rounded">Sign up</button></Link>
             </div>
        </div>
     )
