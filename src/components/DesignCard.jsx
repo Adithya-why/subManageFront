0a8ef6dec5c12e90146ecb07083defe3aa3aa714
Adi <adithiyans123@gmail.com>
2024-06-03 19:18:55 +0530

    styled the landing page

--- src/components/DesignCard.jsx
+++ src/components/DesignCard.jsx
@@ -19,7 +19,7 @@ export default function DesignCard({ user }){
 
     return(
         <div className=" dc flex flex-col items-center w-full h-full pt-20 gap-10  bg-[#F5EEC8] text-green-600">
-            <div className="text-6xl text-center w-2/3">Manage all your subscriptions in one place</div>
+            <div className="text-6xl text-center w-2/3 font-thin">Manage all your <span className="text-[#5aa9e6] font-normal">Subscriptions</span> in one place</div>
 
             <div className="text-xl font-normal text-amber-800 w-1/2 text-center">
                 Subman allows to monitor and track all your subscriptions and memberships in one place and summarise them in the dashboard
@@ -27,7 +27,7 @@ export default function DesignCard({ user }){
 
 
             <div>
-                <button className=" rounded-3xl p-4 text-lg font-bold bg-green-600 text-white"><Link to={startPath}>Get started</Link></button>
+                <button className=" rounded-3xl p-4 text-lg font-bold bg-[#60d394] text-white"><Link to={startPath}>Get started</Link></button>
             </div>
         </div>
     )
